b720b0ead04560e4b13bc759b801d6f110695cbf
khanhpq2 <khanhpq2@tcbs.com.vn>
2021-10-12 18:37:49 +0700

    Update

--- Groovy/1.Syntax.groovy
+++ Groovy/1.Syntax.groovy
@@ -102,11 +102,35 @@ def with_underscore
 def $dollarStart
 
 // keyword sau dấu chấm đều được định danh hợp lệ
+/*
 foo.as
 foo.assert
 foo.break
 foo.case
 foo.catch
+*/
+
+// Quoted identifiers
+def map = [:]
+
+map."an identifier with a space and double quotes" = "ALLOWED"
+map.'with-dash-signs-and-single-quotes' = "ALLOWED"
+
+assert map."an identifier with a space and double quotes" == "ALLOWED"
+assert map.'with-dash-signs-and-single-quotes' == "ALLOWED"
+map.'single quote'
+map."double quote"
+map.'''triple single quote'''
+map."""triple double quote"""
+map./slashy string/
+map.$/dollar slashy string/$
 
 
-// Quoted identifiers (DD dấu chấm)
+def firstname = "Homer"
+map."Simpson-${firstname}" = "Homer Simpson"
+
+assert map.'Simpson-Homer' == "Homer Simpson"
+
+
+///#### Strings
+//
